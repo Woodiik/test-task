bdc593f2bdc300fffc27350401bd574afe34cd04
Woodiik <vovikvova04@gmail.com>
2023-05-20 21:38:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/test-taks">
+    <BrowserRouter basename="/test-task">
       <App />
     </BrowserRouter>
   </React.StrictMode>
